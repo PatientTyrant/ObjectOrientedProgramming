c3caf9346c8cd661e9f7db5a6f5dc29df343c2cc
PatientTyrant <105866700+PatientTyrant@users.noreply.github.com>
2022-11-10 21:50:16 +0300

    Inheritance

---
+++ 4_Inheritance_1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Inheritance_1
+{
+    // inheritance => bir sınıfın başka bir sınıftan türetilmesidir. Mirsa alan sınıg miras veren sınıfın özelliklerini kullanır. Bu sayade ortak modüller tek bi sınıf altında toplanıp diğer sınıflara devreder.
+
+    // miras veren sınıfa base class denir. Mirsa alan sınıfada derived class, child class sub class) gibi isimler verilir. c$ dilinde kalıtım operatörü : 'dir.
+
+    // c# dilinde tekli kalıtım vardır. Bir sınıf yalnızca bir sınıftan türeyebilir.
+
+    public class Insan
+    {
+        public string Adi { get; set; }
+    }
+    //miras alan sınıflar miras veren sınıfların özelliklerini aldıkları gibi kendi özellikleride olabilir.
+    public class Ogrenci : Insan
+    {
+      
+    }
+    public class Hoca : Insan
+    {
+     public decimal Maas { get; set; }
+    }
+}
---
+++ 4_Inheritance_1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _4_Inheritance_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.Adi = "Berat";
+
+            Hoca hc = new Hoca();
+            hc.Adi = "Ekrem";
+            hc.Maas = 500;
+
+            Test(ogr);
+        }
+
+        public static void Test(Ogrenci p)
+        {
+            Console.WriteLine(p.Adi);
+        }
+    }
+}
